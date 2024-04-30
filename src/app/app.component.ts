50299463fb4732e875146fe537b17659ef30c57b
DESKTOP-L3ST01\ADMIN <Shafi.Rahman@theaccessgroup.com>
2024-04-30 18:30:10 +0530

    Fixed Navigation issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { LoginComponent } from '../login/login.component';
 import { RegisterComponent } from '../register/register.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, LoginComponent, RegisterComponent ],
+  imports: [CommonModule, RouterOutlet, LoginComponent, RegisterComponent, RouterLink ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
