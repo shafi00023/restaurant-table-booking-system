6f8fba10bfc63f9106a08f72dc1373521538ed1f
DESKTOP-L3ST01\ADMIN <Shafi.Rahman@theaccessgroup.com>
2024-05-08 11:41:04 +0530

    Issue Fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,24 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterLink, RouterOutlet } from '@angular/router';
-import { LoginComponent } from '../login/login.component';
-import { RegisterComponent } from '../register/register.component';
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterLink, RouterOutlet } from "@angular/router";
+import { LoginComponent } from "../login/login.component";
+import { RegisterComponent } from "../register/register.component";
+import { ApiService } from "./core/api/register/api.service";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
-  imports: [CommonModule, RouterOutlet, LoginComponent, RegisterComponent, RouterLink ],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    LoginComponent,
+    RegisterComponent,
+    RouterLink,
+  ],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.scss",
+  providers: [ApiService],
 })
 export class AppComponent {
-  title = 'restaurant-table-booking-system';
+  title = "restaurant-table-booking-system";
 }
