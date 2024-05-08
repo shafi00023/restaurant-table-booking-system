d627ecb02fc361d2a265d722687db3d71f1c3daa
Aesha Darji <Aesha.Darji@theaccessgroup.com>
2024-05-08 11:44:31 +0530

    login api integration

--- src/login/login.component.ts
+++ src/login/login.component.ts
@@ -1,21 +1,41 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-  constructor(private router: Router) { }
+  constructor(private router: Router,private http: HttpClient) { }
   register() {
       this.router.navigate(["/register"]);
   }
-  login(){
-    this.router.navigate(["/dashboard"]);
+  email: string = '';
+  password: string = '';
+  errorMessage: string = '';
+
+
+  login() {
+    this.http.get<any[]>('/api/users').subscribe(
+      (users) => {
+        const user = users.find(u => u.email === this.email && u.password === this.password);
+        if (user) {
+          console.log('Login successful');
+        } else {
+          this.errorMessage = 'Invalid username or password';
+        }
+      },
+      (error) => {
+        console.error('Error fetching users:', error);
+      }
+    );
   }
 
 }
