dd430d29608880d28380d58815b8a1856798074c
Aesha Darji <Aesha.Darji@theaccessgroup.com>
2024-05-09 17:36:46 +0530

    compelted book-table and dashboard component

--- src/previous-booking/previous-booking.component.ts
+++ src/previous-booking/previous-booking.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { BookingService } from '../app/core/api/book-table/app.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-previous-booking',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './previous-booking.component.html',
   styleUrl: './previous-booking.component.scss'
 })
 export class PreviousBookingComponent {
+  bookingData: any;
 
+  constructor(private bookingService: BookingService) { }
+
+  ngOnInit(): void {
+    this.loadBookingData();
+  }
+
+  loadBookingData() {
+    this.bookingService.getAllBooking().subscribe(
+      data => {
+        this.bookingData = data;
+      },
+      error => {
+        console.error('Failed to fetch booking data:', error);
+      }
+    );
+  }
 }
