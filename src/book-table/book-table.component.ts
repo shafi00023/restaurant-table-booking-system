9e923ecee325c052b200c9794d4822a0fdaf4d76
DESKTOP-L3ST01\ADMIN <Shafi.Rahman@theaccessgroup.com>
2024-05-21 19:02:55 +0530

    Fixed

--- src/book-table/book-table.component.ts
+++ src/book-table/book-table.component.ts
@@ -9,48 +9,61 @@ interface BookingData {
   email: string;
   date: string;
   time: string;
-  partySize: number; 
+  partySize: number;
 }
 
 @Component({
   selector: "app-book-table",
   standalone: true,
-  imports: [FormsModule, CommonModule, SidebarComponent,HeaderComponent],
+  imports: [FormsModule, CommonModule, SidebarComponent, HeaderComponent],
   templateUrl: "./book-table.component.html",
   styleUrl: "./book-table.component.scss",
 })
 export class BookTableComponent {
   bookingData: BookingData = {
-    name: '',
-    email: '',
-    date: '',
-    time: '',
-    partySize: 0
+    name: "",
+    email: "",
+    date: "",
+    time: "",
+    partySize: 0,
   };
-  availableTables: string[] = [];
+  availableTables: number[] = [];
   selectedTable: string | null = null;
   constructor(private bookingService: BookingService) {}
 
+  // checkAvailability(): void {
+  //   if (this.bookingData.date && this.bookingData.time && this.bookingData.partySize > 0) {
+  //     console.log("availableTables",this.availableTables);
+  //     this.bookingService.checkAvailability(this.bookingData).subscribe(tables => {
+  //       this.availableTables = tables;
+  //     });
+  //   }
+  // }
   checkAvailability(): void {
-    if (this.bookingData.date && this.bookingData.time && this.bookingData.partySize > 0) {
-      console.log("availableTables",this.availableTables);
-      this.bookingService.checkAvailability(this.bookingData).subscribe(tables => {
+    const { date, time, partySize } = this.bookingData;
+    this.bookingService.checkAvailability(date, time, partySize).subscribe(
+      (tables: number[]) => {
         this.availableTables = tables;
-      });
-    }
+      },
+      (error) => {
+        console.error("Failed to check availability:", error);
+      }
+    );
   }
 
   onSubmit(): void {
     if (this.selectedTable) {
       const bookingDataWithTable = {
         ...this.bookingData,
-        tableNo: this.selectedTable
+        tableNo: this.selectedTable,
       };
-      this.bookingService.bookTable(bookingDataWithTable).subscribe(response => {
-        alert('Booking confirmed!');
-      });
+      this.bookingService
+        .bookTable(bookingDataWithTable)
+        .subscribe((response) => {
+          alert("Booking confirmed!");
+        });
     } else {
-      alert('Please select a table before booking.');
+      alert("Please select a table before booking.");
     }
   }
   clearInputs() {
@@ -62,5 +75,4 @@ export class BookTableComponent {
       partySize: 0,
     };
   }
-
 }
